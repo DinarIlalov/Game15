0f82709bed1ae1f538089df7f84dafd8be4cd4d7
Dinar <dinarilalov@mail.ru>
2019-04-10 10:18:20 +0300

    init

---
+++ Game15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game15
+//
+//  Created by Dinar Ilalov on 10/04/2019.
+//  Copyright © 2019 Dinar Ilalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
